347f44a60f0574620606dc0c296204fac017c012
Kristof-oe <kris200036@gmail.com>
2023-10-31 20:48:37 +0100

    Add project files.

---
+++ Git_seged_3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_seged_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
