e8a19435deb76165e61b27b247c468f4487754dd
Kristof-oe <kris200036@gmail.com>
2023-10-31 20:51:03 +0100

    hft teszt

---
+++ Git_seged_3/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_seged_3
+{
+    internal class Person
+    {
+        public string Nev { get; set; } 
+        public int Kor {get; set; }
+
+        public Person(string nev, int kor)
+        {
+            Nev = nev;
+            Kor = kor;
+        }
+    }
+}
